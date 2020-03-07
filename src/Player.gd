7954562d4b94e5e4d4df04e51d12902cccccfa68
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-07 00:33:50 -0300

    add tileset and update grid movement

--- src/Player.gd
+++ src/Player.gd
@@ -2,29 +2,21 @@ extends KinematicBody2D
 
 const CELL_SIZE = 8
 const SPEED = 64
+const CELL_VECTOR = Vector2(CELL_SIZE, CELL_SIZE)
 
 onready var sprite = $Sprite
 
-var last_position = Vector2.ZERO
-var target_position = Vector2.ZERO
-var movement_direction = Vector2.ZERO
+var movement_direction = Vector2.RIGHT
 
 func _ready():
-    position = position.snapped(Vector2(CELL_SIZE, CELL_SIZE))
-    last_position = position
-    target_position = position
+    position = position.snapped(CELL_VECTOR)
 
 func _physics_process(delta):
-    position += movement_direction * SPEED * delta
+    move_and_slide(movement_direction * SPEED)
 
-    if position.distance_to(last_position) >= CELL_SIZE - SPEED * delta:
-        position = target_position
+    update_movement_dir()
 
-    if position == target_position:
-        update_movement_dir()
-        last_position = position
-        target_position += movement_direction * CELL_SIZE
-
+func _process(delta):
     update_sprite_rotation()
 
 func update_movement_dir():
@@ -33,19 +25,24 @@ func update_movement_dir():
     var up = Input.is_action_pressed("ui_up")
     var down = Input.is_action_pressed("ui_down")
 
-    movement_direction.x = int(right) - int(left)
-    movement_direction.y = int(down) - int(up)
+    var new_move = Vector2(int(right) - int(left), int(down) - int(up))
 
-    if movement_direction.x != 0 and movement_direction.y != 0:
-        movement_direction = Vector2.ZERO
+    if (up or down) != (left or right):
+        position = position.snapped(CELL_VECTOR)
+        movement_direction = new_move
 
 func update_sprite_rotation():
     if movement_direction.x != 0:
         sprite.rotation = 0
+        sprite.position = Vector2.ZERO
         sprite.flip_h = movement_direction.x < 0
     elif movement_direction.y != 0:
         sprite.flip_h = false
         if movement_direction.y > 0:
+            sprite.position.x = 8
+            sprite.position.y = 0
             sprite.rotation_degrees = 90
         else:
+            sprite.position.x = 0
+            sprite.position.y = 8
             sprite.rotation_degrees = -90
